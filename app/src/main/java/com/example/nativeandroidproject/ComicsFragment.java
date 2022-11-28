bacc1976a0d6dd033991826a12d366af05cae0ce
gabrielgimenes92 <gabrielcgimenes@gmail.com>
2022-11-28 01:00:14 -0800

    Adds fragments and tab layout

---
+++ app/src/main/java/com/example/nativeandroidproject/ComicsFragment.java
@@ -0,0 +1,19 @@
+package com.example.nativeandroidproject;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ComicsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_comics, container, false);
+    }
+}
